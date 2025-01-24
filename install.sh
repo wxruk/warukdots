d53e01536648ec187eed3ffdf891a3c43eec4335
wxruk <118911112+wxruk@users.noreply.github.com>
2025-01-24 13:48:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yay -S python-pywal16 neovide neovim hyprland swaync swww grim kitty chromium
-git clone https://github.com/warukoff/warukdots
+git clone https://github.com/wxruk/warukdots
 cp -r warukdots/config/* ~/.config/
 cp warukdots/zshrc ~/.zshrc 
 cp -r .oh-my-zsh ~/.oh-my-zsh
